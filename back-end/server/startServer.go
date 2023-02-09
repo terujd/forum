0ed197b75a6b5b3afe00507e6faaefd2c72be58b
mijan <jansson.michael.93@gmail.com>
2023-02-09 20:04:49 +0200

    lol

--- back-end/server/startServer.go
+++ back-end/server/startServer.go
@@ -10,7 +10,7 @@ var Port = "1337"
 
 func StartServer() {
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./front-end/css/main.css"))))
-	fmt.Println("Server started on port", Port)
+	fmt.Println("Elite server started on port", Port)
 
 	err := http.ListenAndServe(":"+Port, nil)
 	if err != nil {
